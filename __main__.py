cf7ae26aac1c1e8f722acda1dd22156015146c19
babimtero <babak.izadpanah@gmail.com>
2024-02-11 10:44:25 +0330

    message

--- __main__.py
+++ __main__.py
@@ -1,15 +1,27 @@
 
+#============================================================
+#libraries
 import ini
-import pymssql
-import datetime
-import json
-import os
-import post
 import enc
+import datetime
+'''Hesam jan I stored key in ./molo/molo.pnvt file , 
+you can put anything, every where you want
+you can also ask user to enter any time'''
+
 with open('./molo/molo.pnvt', 'rb') as filekey:
 	key = filekey.read()
- 
+#=========================================================    
+
+'''Now you have your config file'''
 filename='Configuration.ini'
+#============================================
+'''
+you encypt it for first initiation time.
+next time you runn the program you make as commment this part,
+only for first time or config change you run it.'''
+enc.encrypt_fernet(key,filename)
+#========================================
+'''This is for '''
 
 enc.decrypt_fernet(key,filename)
 config = ini.parse(open(filename).read())
@@ -22,44 +34,11 @@ today=str( tod-datetime.timedelta(days = delta))
 
 
 print("Initialization Done")  
-url='.\\jason\\'
-filelist=os.listdir(url)
-for jfile in filelist:
-
-    jf = open(url+jfile)
-    dataa=json.load(jf)
-    jsone=json. loads(post.create(dataa))
-    print(jfile,jsone)
-    
-    success=jsone["success"]
-    message=jsone["message"]
-    result=jsone["result"]['tracking']
-    
-    
-    print('success=',success)
-    print("message=",message)
-    print("result=",result)
-    
-    
-    if success==True:
-    
-        connection= pymssql.connect(server=config['source']['ios']['server'], user=config['source']['ios']['user'], password=config['source']['ios']['password'], database=config['source']['ios']['database'])
-    
-        cur = connection.cursor()
-        update_list_sql="""UPDATE [dbo].[CRA_EI_Tracker]
-              SET [CRA_TrackingNo] = %s
-                  ,[Raise_Date]=%s
-                  ,[Modified_By]='shakila.k@mtnirancell.ir'
-              where [MVO_TrackingNo]=%s"""
-        f=len(jfile)
-        k=f-5
-        Mvo_Trackingcode=jfile[0:k]
-        cur.execute(update_list_sql,(result,today,Mvo_Trackingcode))
-        connection.commit()
-    
-    
-    print(Mvo_Trackingcode,jsone)
-    
-    print('Terminated becase of above Error')
+print(today)
+server=config['source']['ios']['server']
+user=config['source']['ios']['user']
+password=config['source']['ios']['password']
+database=config['source']['ios']['database']
+print(server,user,password,database)
     
 
